601221460ffc547745e22c95128ba144fadb2ef2
Amberly <ahoffman@miletwo.us>
2018-05-10 13:51:28 -0400

    slider working boo-yah

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -1,21 +1,19 @@
 //jQuery checker [debug tool]
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
 
-//slideshow preferences 
+//slideshow preferences
 $(document).ready(function(){
   $('.slideshow_container').slick({
-    arrows: true,
-    slidesToShow: 4,
-    dots: true,
-    centerMode: true,
+  swipeToSlide: true,
+  arrows: false,
   });
 });
 
