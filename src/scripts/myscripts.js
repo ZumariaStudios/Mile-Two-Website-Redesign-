d38eee96c1be0271688281088d28f68032481bb9
Amberly <ahoffman@miletwo.us>
2018-05-10 11:43:39 -0400

    pre J query

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  $('.slideshow_container').slick();
+  $('.slideshow_container').slick({
+    arrows: true,
+    slidesToShow: 4,
+    dots: true,
+    centerMode: true;
+  });
 });
