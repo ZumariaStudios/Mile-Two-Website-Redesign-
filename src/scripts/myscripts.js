3aae742c4395259de01b9ac18cbba6cd776a0444
Amberly <ahoffman@miletwo.us>
2018-05-10 13:00:07 -0400

    jQuery working

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -1,8 +1,22 @@
+//jQuery checker [debug tool]
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
+
+//slideshow preferences 
 $(document).ready(function(){
   $('.slideshow_container').slick({
     arrows: true,
     slidesToShow: 4,
     dots: true,
-    centerMode: true;
+    centerMode: true,
   });
 });
+
+console.log('this is a console log');
