de262f30c7b78ca32f2bb0eee3b19f311b645508
Amberly <ahoffman@miletwo.us>
2018-05-10 11:06:45 -0400

    Progress toward slick

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-  $('.slideshow_container').slick({
-    arrows: true,
-    slidesToShow: 4,
-    dots: true,
-    centerMode: true;
-  });
+  $('.slideshow_container').slick();
 });
