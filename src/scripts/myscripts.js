bf53fddd0d00ad1117f1162ea3e809769135630d
Amberly <ahoffman@miletwo.us>
2018-05-15 10:58:18 -0400

    Carousel working beginning SVG background

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -1,13 +1,13 @@
 //jQuery checker [debug tool]
-window.onload = function() {
-    if (window.jQuery) {
-        // jQuery is loaded
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
 
 //slideshow preferences
 $(document).ready(function(){
