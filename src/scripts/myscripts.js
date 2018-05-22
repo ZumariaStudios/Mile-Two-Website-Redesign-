99edfc08edbf1c7f9e35ec88850b0efe70d5fe00
Milu Franz <mfranz@miletwo.us>
2018-05-22 08:39:33 -0400

    changed header title depending on page

--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -33,6 +33,45 @@ $(document).ready(function(){
   });
 });
 
+// slideshow preferences in complete team
+$(document).ready(function(){
+  $('.sliderTeam').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+});
+
 // reveal preferences
 // window.sr = ScrollReveal({
 //   reset: true
