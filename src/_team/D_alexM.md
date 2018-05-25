785b1ae0a71f8847fcf672fa0d28e4a022fc2a05
Milu Franz <mfranz@miletwo.us>
2018-05-25 12:21:15 -0400

    added navigation button to bios

--- src/_team/D_alexM.md
+++ src/_team/D_alexM.md
@@ -1,8 +1,10 @@
 ---
 name: Alex Morrison, Ph.D
 short_name: ALEX
-title: Senior Cognitive Systems Engineer
+title: Chief Science Officer
 image: AlexM.png
 layout: person
 ---
-Lorem ipsum dolor sit amet, consectetuer adipiscing consectetuer adipiscing amet, consectetuer adipiscing consectetuer adipiscin blatuer adipiscing consectetuer a bla. Click here
+Alex brings a background in computer vision, human perception, and cognitive systems engineering to investigate sense-making, perception, and attention principles in various domains including cybersecurity, visual analytics in intelligence analysis, and human-machine teaming robots. As a Cognitive Systems Engineer (CSE), Dr. Morison has a rich set of experiences and expertise in CTA techniques, representation aiding, and scenario development.
+
+* [LinkedIn](https://www.linkedin.com/in/alexmorison/)
--- src/scripts/myscripts.js
+++ src/scripts/myscripts.js
@@ -37,11 +37,8 @@ $(document).ready(function(){
 // slideshow preferences in bio pages
 $(document).ready(function(){
   $('.personal_slider').slick({
-    centerMode: true,
     dots: true,
     speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
     swipeToSlide: true,
     arrows: false,
   });
@@ -73,8 +70,8 @@ $(document).ready(function(){
     slidesToScroll: 3,
     prevArrow: false,
     nextArrow: false,
-    autoplay: false,
-    autoplaySpeed: 2500,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1024,
@@ -108,43 +105,41 @@ $(document).ready(function(){
 // });
 // sr.reveal('.test', {opacity: 0.9,duration:3000});
 
-var $animation_elements = $('.animation-element');
-var $window = $(window);
+$(document).ready(function(){
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
 
-for (var key in $animation_elements) {
-  console.log(key, $animation_elements[key]);
-}
+  function check_if_in_view() {
+    var window_height = $window.height();
+    console.log('window height: ' + window_height);
 
-function check_if_in_view() {
-  var window_height = $window.height();
-  console.log('window height: ' + window_height);
+    var window_top_position = $window.scrollTop();
+    console.log('window top position: ' + window_top_position);
 
-  var window_top_position = $window.scrollTop();
-  console.log('window top position: ' + window_top_position);
+    var window_bottom_position = (window_top_position + window_height);
+    console.log('window bottom position: ' + window_bottom_position);
 
-  var window_bottom_position = (window_top_position + window_height);
-  console.log('window bottom position: ' + window_bottom_position);
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      console.log('element height: ' + element_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    console.log('element height: ' + element_height);
+      var element_top_position = $element.offset().top;
+      console.log('element top position: ' + element_top_position);
 
-    var element_top_position = $element.offset().top;
-    console.log('element top position: ' + element_top_position);
+      var element_bottom_position = (element_top_position + element_height);
+      console.log('element bottom position: ' + element_bottom_position);
 
-    var element_bottom_position = (element_top_position + element_height);
-    console.log('element bottom position: ' + element_bottom_position);
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+});
